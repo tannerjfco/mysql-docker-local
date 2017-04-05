c490a9a46de1ca65dacd62fec8083177923d5706
Tanner J Ferguson <tannerjfco@gmail.com>
2017-04-05 08:24:54 -0700

    suppress mysql warning on db import (#10)

--- 5.7/files/import.sh
+++ 5.7/files/import.sh
@@ -3,8 +3,8 @@
 # if a sql dump has been mounted into /db then import it
 dbfile="/db/data.sql"
 if [ -f "$dbfile" ]; then
-    echo "importing $dbfile"
-    mysql -u root -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE -e "SOURCE $dbfile"
+    echo "Importing database"
+    MYSQL_PWD=$MYSQL_ROOT_PASSWORD mysql -u root $MYSQL_DATABASE -e "SOURCE $dbfile"
 else
     echo "no file to import found at $dbfile"
 fi
