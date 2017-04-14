282ad4fa819e033b1e2f30b6540a44c170765a37
Tanner J Ferguson <tannerjfco@gmail.com>
2017-04-14 17:54:42 -0600

    update import for multi-file and return exit code if none (#11)
    * update import for multi-file and return exit code if none
    
    * improve multi file import

--- 5.7/files/import.sh
+++ 5.7/files/import.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-
-# if a sql dump has been mounted into /db then import it
 dbfile="/db/data.sql"
-if [ -f "$dbfile" ]; then
-    echo "Importing database"
-    MYSQL_PWD=$MYSQL_ROOT_PASSWORD mysql -u root $MYSQL_DATABASE -e "SOURCE $dbfile"
+files=`find /db -name '*.sql'`
+
+# if sql dump(s) have been mounted into /db combine and import them
+if [ -n "$files" ]; then
+    echo "Importing database..."
+    find /db -name '*.sql' | awk '{ print "source",$0 }' | MYSQL_PWD=$MYSQL_ROOT_PASSWORD mysql -u root $MYSQL_DATABASE --batch
+    rm /db/*.sql
 else
-    echo "no file to import found at $dbfile"
+    echo "No file to import found."
+    exit 1
 fi
