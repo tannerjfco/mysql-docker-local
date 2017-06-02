7bba85354258e36a29f22e44c4bd7b46eb795e1d
tannerjfco <tannerjfco@gmail.com>
2017-06-02 16:18:30 -0600

    fixes user credentials on mysql import

--- 5.7/files/import.sh
+++ 5.7/files/import.sh
@@ -5,7 +5,7 @@ files=`find /db -name '*.sql'`
 # if sql dump(s) have been mounted into /db combine and import them
 if [ -n "$files" ]; then
     echo "Importing database..."
-    find /db -name '*.sql' | awk '{ print "source",$0 }' | MYSQL_PWD=$MYSQL_ROOT_PASSWORD mysql -u root $MYSQL_DATABASE --batch
+    find /db -name '*.sql' | awk '{ print "source",$0 }' | mysql --batch
     rm /db/*.sql
 else
     echo "No file to import found."
